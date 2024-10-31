d29e02c0965b324211ac2eac26ae3d4aeaca3550
adesh malunjkar <3636adesh@gmail.com>
2024-10-31 20:28:54 +0530

    minor changes

--- patterns/src/main/java/com/example/features/forkjoin/UsingForkJoinFramework.java
+++ patterns/src/main/java/com/example/features/forkjoin/UsingForkJoinFramework.java
@@ -4,4 +4,12 @@ import com.example.Adesh;
 
 @Adesh
 public class UsingForkJoinFramework {
+
+
+        public static void main(String[] args) {
+            int cores = Runtime.getRuntime().availableProcessors();
+            System.out.println("Number of platform threads supported: " + cores);
+        }
+
+
 }
